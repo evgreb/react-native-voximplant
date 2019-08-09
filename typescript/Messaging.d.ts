66957f426592dad2067d5aa4e5185facff08c407
Евгений Гребенщиков <evgreb@gmail.com>
2019-08-09 20:10:26 +0300

    add class Message

--- typescript/Messaging.d.ts
+++ typescript/Messaging.d.ts
@@ -153,7 +153,6 @@ declare module "react-native-voximplant" {
         owner?: boolean;
     }
 
-
     /*
     * Interface that represents user information. Voximplant users are created via the Voximplant control panel or HTTP API.
     * */
@@ -207,8 +206,72 @@ declare module "react-native-voximplant" {
 
     namespace Voximplant {
         namespace Messaging {
-            export class Conversation{} //TODO realise class
-            export class Message{} //TODO realise
+            export class Conversation {} //TODO realise class
+
+
+            /*
+            * Interface that represents a message within a conversation.
+            * */
+            export class Message {
+
+                /*
+                * The UUID of the conversation this message belongs to. The message can belong to the one conversation only.
+                * */
+                conversation: string;
+
+                /*
+                * The array of payload objects associated with the message.
+                * */
+                payload: any[];
+
+                /*
+                * The message sequence number in the conversation.
+                * */
+                sequence: number;
+
+                /*
+                * The text of this message.
+                * */
+                text: string;
+
+                /*
+                * The universally unique identifier (UUID) of the message.
+                * */
+                uuid: string;
+
+                /*
+                * Remove the message from the conversation.
+                *
+                * The participant that calls this method should have:
+                * - the {@link Voximplant.Messaging.ConversationParticipant#canRemoveMessages} permission to remove its own messages
+                * - the {@link Voximplant.Messaging.ConversationParticipant#canRemoveAllMessages} permission to remove other participants' messages
+                *
+                * Other parties of the conversation (online participants and logged in clients) can be informed about
+                * the message removing via the {@link Voximplant.Messaging.MessengerEventTypes.RemoveMessage} event.
+                *
+                * */
+                remove(): Promise<MessageEvent|ErrorEvent>;
+
+                /*
+                * Send text and payload changes to the cloud.
+                *
+                * The participant that calls this method should have:
+                * - the {@link Voximplant.Messaging.ConversationParticipant#canEditMessages} permission to update its own messages
+                * - the {@link Voximplant.Messaging.ConversationParticipant#canEditAllMessages} permission to update other participants' messages
+                *
+                * Other parties of the conversation (online participants and logged in clients) can be informed about the message
+                * updating via the {@link Voximplant.Messaging.MessengerEventTypes.EditMessage} event.
+                *
+                * To be informed about the message updating while being offline, participants can subscribe
+                * to the {@link Voximplant.Messaging.MessengerNotification.EditMessage} messenger push notification.
+                *
+                * @param {string} text - New text of this message, maximum 5000 characters. If null, message text will not be updated.
+                * @param {Array<object>} payload - New payload of this message. If null, message payload will not be updated.
+                * */
+                update(text: string, payload: Array<object>): Promise<MessageEvent|ErrorEvent>
+            }
+
+            export class Messenger {} //TODO realise
 
             /*
             * Enum that represents actions that trigger messenger events. Each action is the reason for every triggered event.
