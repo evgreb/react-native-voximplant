df28e237f0dfdae76210379c0f3094e87398f048
Евгений Гребенщиков <evgreb@gmail.com>
2019-08-09 15:23:01 +0300

    add StatusEvent handler interface

--- typescript/EventHandlers.d.ts
+++ typescript/EventHandlers.d.ts
@@ -655,4 +655,26 @@ declare module "react-native-voximplant" {
         * */
         to: number
     }
+    export interface StatusEvent {
+
+        /*
+        * Action that triggered this event.
+        * */
+        action: Voximplant.MessengerAction,
+
+        /*
+        * Messenger event type.
+        * */
+        eventType: Voximplant.MessengerEventTypes,
+
+        /*
+        * The IM id for the user that initiated the event.
+        * */
+        imUserId: number,
+
+        /*
+        * The user status.
+        * */
+        online: boolean
+    }
 }
